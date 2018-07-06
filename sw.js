db6b3bfc8b1fbb6e92e858b1ec72b069aa26f45c
Charles <chuckmossiii@gmail.com>
2018-07-06 16:16:46 -0500

    Fixed invalid entry with service worker and fixed deferred style sheet

--- sw.js
+++ sw.js
@@ -1,38 +1,8 @@
-class browserDB {
-
-    constructor() {
-        this.indexedDB = ServiceWorkerGlobalScope.indexedDB;
-    }
-
-    /*
-     * Open database and set callbacks
-     */
-    access(success) {
-        this.open = this.indexedDB.open("RestaurantReviewApp", 1);
-        this.open.onupgradeneeded = () => {
-            let db = this.open.result;
-            db.createObjectStore("RestaurantReviewApp");
-        };
-        this.open.onsuccess = () => {
-            let db = this.open.result;
-            let tx = db.transaction("RestaurantReviewApp", "readwrite");
-            let store = tx.objectStore("RestaurantReviewApp");
-            this.successCallback(store);
-            tx.oncomplete = function() {
-                db.close();
-            };
-        };
-        this.successCallback = success;
-    }
-
-}
-
-let dbInstance = null;
 const syncStore = {};
 const CACHE_NAME = 'my-site-cache-v1';
 const urlsToCache = [
     '/',
-    '/css/styles.min.css',
+    '/css/styles.css',
     '/js/common.js',
     '/js/browserDB.js',
     '/js/dbhelper.js',
@@ -56,7 +26,6 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('activate', function (event)
 {
-    dbInstance = new browserDB();
     event.waitUntil(self.clients.claim());
 });
 
